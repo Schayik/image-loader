e0d0a3d32b0813d09ceea8b3a27ec3eb12993400
Pim van Schayik <pimvschayik@live.nl>
2020-02-20 12:33:36 +0000

    del: css

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, waitForElement } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect'
 // import axiosMock from 'axios'
-import App from './App';
+import App from './components/App';
 
 // jest.mock('axios')
 
