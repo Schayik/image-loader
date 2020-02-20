a38ec31276c39595c3751648e3f99fa47c32310e
Pim van Schayik <pimvschayik@live.nl>
2020-02-20 10:03:56 +0000

    feat: prev and next with memoizing

--- src/App.js
+++ src/App.js
@@ -1,37 +1,63 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import axios from 'axios'
 
-const App = () => {
-  
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'addPage':
+      return {
+        ...state,
+        [action.page]: action.hits,
+      }
+    default:
+      return state
+  }
+}
 
-  const [data, setData] = useState('')
+const App = () => {
+
+  const [state, dispatch] = useReducer(reducer, {})
+  const [page, setPage] = useState(1)
+
+  const handleClick = newPage => {
+    if (newPage < 1) {
+      setPage(1)
+    } else if (newPage > 25) {
+      setPage(25)
+    } else {
+      setPage(newPage)
+    }
+  }
   // const [buttonClicked, setButtonClicked] = useState(false)
 
-  useEffect(() => {
-    const url = 'https://pixabay.com/api/?key=' + process.env.REACT_APP_PIXABAY_KEY
 
+
+  useEffect(() => {
     const fetchImages = async () => {
+      const url = `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_KEY}&page=${page}`
+  
       const response = await axios.get(url)
-      console.log(response.data)
-      setData(response.data)
+  
+      dispatch({
+        type: 'addPage',
+        hits: response.data.hits,
+        page,
+      })
     }
 
-    fetchImages()
-  }, [])
+    if (!state[page]) {
+      fetchImages()
+    }
+  }, [page])
 
-  // const fetchImages = async () => {
-  //   const response  = await axios.get(url)
-  //   const data = await response
-  //   setData(data)
-  //   // setButtonClicked(true)
-  // }
+  console.log(state)
 
   // if (!data) return <div>loading...</div>
 
   return (
     <div>
-      {/* <button onClick={fetchImages}>Load Images</button> */}
-      {data && <p data-testid='json'>{JSON.stringify(data)}</p>}
+      <button onClick={() => handleClick(page - 1)}>Prev</button>
+      <button onClick={() => handleClick(page + 1)}>Next</button>
+      {state[page] && <p data-testid='json'>{JSON.stringify(state[page])}</p>}
     </div>
   );
 }
