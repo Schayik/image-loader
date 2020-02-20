534cd128586d70a31ba3140f226621c4ac722787
Pim van Schayik <pimvschayik@live.nl>
2020-02-20 09:36:08 +0000

    test: wrote simple one

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
-function App() {
+const App = () => {
+  
+
+  const [data, setData] = useState('')
+  // const [buttonClicked, setButtonClicked] = useState(false)
+
+  useEffect(() => {
+    const url = 'https://pixabay.com/api/?key=' + process.env.REACT_APP_PIXABAY_KEY
+
+    const fetchImages = async () => {
+      const response = await axios.get(url)
+      console.log(response.data)
+      setData(response.data)
+    }
+
+    fetchImages()
+  }, [])
+
+  // const fetchImages = async () => {
+  //   const response  = await axios.get(url)
+  //   const data = await response
+  //   setData(data)
+  //   // setButtonClicked(true)
+  // }
+
+  // if (!data) return <div>loading...</div>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <button onClick={fetchImages}>Load Images</button> */}
+      {data && <p data-testid='json'>{JSON.stringify(data)}</p>}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitForElement } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'
+// import axiosMock from 'axios'
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// jest.mock('axios')
+
+test('test app', async () => {
+  
+  const { getByTestId } = render(<App />);
+
+  // axiosMock.get.mockResolvedValueOnce({
+  //   data: { greeting: 'totalhits' },
+  // })
+
+  // fireEvent.click(getByText('Load Images'))
+
+  await waitForElement(() => getByTestId('json'))
+
+  // expect(axiosMock.get).toHaveBeenCalledTimes(1)
+  // expect(axiosMock.get).toHaveBeenCalledWith(url)
+  expect(getByTestId('json')).toHaveTextContent(/"totalHits":500/)
+  // expect(getByRole('button')).toHaveAttribute('disabled')
 });
